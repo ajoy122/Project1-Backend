19d08f8daf3adca3fb116887568ef16fd4a2f420
ajoy122 <55699077+ajoy122@users.noreply.github.com>
2020-08-01 13:03:17 -0400

    commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+// const body_parser = require('body-parser')
+// const user_routes = require('./routes/users')
 
 const app = express();
+app.use(morgan('dev'))
+// app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.end()
+    
+  })
 
 app.listen(3333, () => {
     console.log('The server is running on port 3333')
