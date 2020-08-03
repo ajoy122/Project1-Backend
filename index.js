17cd822e29eef8bc0afc27763de4807e4f154e7d
ajoy122 <55699077+ajoy122@users.noreply.github.com>
2020-08-03 03:18:48 -0400

    commit

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const morgan = require('morgan')
-// const body_parser = require('body-parser')
-// const user_routes = require('./routes/users')
+const body_parser = require('body-parser')
+const user_routes = require('./routes/users')
 
 const app = express();
 app.use(morgan('dev'))
-// app.use(express.json())
+app.use(express.json())
+app.use(body_parser.urlencoded({extended: false}))
+app.use(user_routes)
+app.use(express.static('./public'));
 
 app.get('/', (request, response) => {
     response.end()
     
   })
 
+  app.get('/users', (request, response) => {
+    response.end()
+    
+  })
 app.listen(3333, () => {
     console.log('The server is running on port 3333')
   })
---
+++ routes/users.js
@@ -0,0 +1,53 @@
+/**
+ * Contains all user related Routes
+ */
+const mysql = require('mysql')
+const express = require('express');
+const { request, response } = require('express');
+const router = express.Router()
+module.exports = router;
+
+function getNewConnection() {
+    return mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: '202004_im215_group1',
+    })
+    }
+
+// create user
+router.post('/user', (request,response) =>{
+   
+    console.log(request.body)
+    console.log(request.body.first_name)
+    response.sendStatus(501)
+})
+
+// update user
+router.post('/user', (request,response) =>{
+    console.log('from patch')
+    console.log(request.body)
+    console.log(request.body.first_name)
+    response.sendStatus(501)
+})
+
+// return list of all users
+router.get('/users', (request,response) =>{
+    const connection = getNewConnection()
+    const queryString = 'select * from `users_signup'
+    // console.log(queryString)
+    connection.query(queryString, (err, rows, fields) => {
+        console.log('Got Response from Database Server')
+        if (err != null) {
+          console.error(err);
+          response.sendStatus(500);
+        }
+    
+        console.log(rows)
+        response.json(rows)
+      })
+      console.log('defined qery and calll back')
+    // response.sendStatus(501)
+})
